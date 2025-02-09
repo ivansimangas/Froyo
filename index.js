c6926c9bad53192e2e6ebf2ca9856e1f30710639
Ivan Simangas <simangasivan@gmail.com>
2025-02-09 05:19:32 -0800

    finished hw

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 function trackFroyoOrder() {
   const userInput = prompt("Enter your froyo flavors (comma seperated):");
-  console.log(userInput);
+  const flavorArray = userInput.split(",");
+  const flavorCount = {};
+  for (const flavor of flavorArray) {
+    if (flavorCount[flavor]) {
+      flavorCount[flavor]++;
+    } else {
+      flavorCount[flavor] = 1;
+    }
+  }
+  console.log(flavorCount);
 }
+
+trackFroyoOrder();
